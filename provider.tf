0519c04c7096bfbb61f6b43a87a5d8bc0c7a398d
Liel <liel.frank@gmail.com>
2026-06-20 17:46:49 +0300

    feat(terraform): add Kubernetes, Helm and kubectl providers

--- provider.tf
+++ provider.tf
@@ -6,6 +6,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.0"
+    }
   }
 
   backend "s3" {
@@ -14,4 +26,30 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+}
+
+# Short-lived token used by helm/kubernetes/kubectl providers to authenticate to the EKS cluster
+data "aws_eks_cluster_auth" "main" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
 }
