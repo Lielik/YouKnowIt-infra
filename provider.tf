52cf0e82f6e3bb605fb82dec658713af369153e8
Liel <liel.frank@gmail.com>
2026-06-20 17:52:45 +0300

    fix(terraform): use correct syntax for helm provider

--- provider.tf
+++ provider.tf
@@ -34,7 +34,7 @@ data "aws_eks_cluster_auth" "main" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
     token                  = data.aws_eks_cluster_auth.main.token
